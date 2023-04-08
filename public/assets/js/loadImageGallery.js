7a8bac1146f3a514e04412ce6fce7f03659db288
cirillojon <104590083+cirillojon@users.noreply.github.com>
2023-04-08 11:34:02 -0400

    updated photo gallery

--- public/assets/js/loadImageGallery.js
+++ public/assets/js/loadImageGallery.js
@@ -1,19 +1,18 @@
 async function loadImageGallery() {
-    try {
-      const response = await fetch('/images');
-      const imageURLs = await response.json();
-      const imageGallery = document.getElementById('image-gallery');
-      imageURLs.forEach((url) => {
-        const img = document.createElement('img');
-        img.src = url;
-        img.alt = ''; 
-        img.className = 'gallery-image';
-        imageGallery.appendChild(img);
-      });
-    } catch (error) {
-      console.error('Error loading image gallery:', error);
-    }
+  try {
+    const response = await fetch('/images');
+    const imageURLs = await response.json();
+    const imageGallery = document.getElementById('image-gallery');
+    imageURLs.forEach((url) => {
+      const img = document.createElement('img');
+      img.src = url;
+      img.alt = '';
+      img.className = 'gallery-image';
+      imageGallery.appendChild(img);
+    });
+  } catch (error) {
+    console.error('Error loading image gallery:', error);
   }
-  
-  loadImageGallery();
-  
+}
+
+loadImageGallery();
